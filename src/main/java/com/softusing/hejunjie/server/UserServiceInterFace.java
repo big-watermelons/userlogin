2e6d4c79e555ea1a980bed198448f3bcd040955f
贺俊杰 <105837287+big-watermelons@users.noreply.github.com>
2023-05-19 14:42:49 +0900

    Add files via upload

---
+++ src/main/java/com/softusing/hejunjie/server/UserServiceInterFace.java
@@ -0,0 +1,15 @@
+package com.softusing.hejunjie.server;
+
+import com.softusing.hejunjie.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+public interface UserServiceInterFace {
+
+    int save(User user);
+
+    boolean login(User user);
+
+}
